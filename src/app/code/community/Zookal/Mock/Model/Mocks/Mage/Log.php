a0c84c65150885d1b842230ec34c4871c1ab36f4
Cyrill Schumacher <cyrill@schumacher.fm>
2014-03-07 14:05:09 +1100

    Update comments

--- src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Log.php
+++ src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Log.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @category    Zookal_Mock
+ * @package     Helper
+ * @author      Cyrill Schumacher | {firstName}@{lastName}.fm | @SchumacherFM
+ * @copyright   Copyright (c) Zookal Pty Ltd
+ * @license     OSL - Open Software Licence 3.0 | http://opensource.org/licenses/osl-3.0.php
+ */
 class Zookal_Mock_Model_Mocks_Mage_Log extends Zookal_Mock_Model_Mocks_Abstract
 {
 
