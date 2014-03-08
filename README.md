a2d18f6b8c0d26a545f542f2194fc0705b8cbe2b
Cyrill Schumacher <cyrill@schumacher.fm>
2014-03-08 13:05:28 +1100

    Update descriptions / readme

--- README.md
+++ README.md
@@ -67,6 +67,27 @@ Add in your custom app/etc/modules/customer_module.xml
 </config>
 ```
 
+I've disabled module FooBar but still getting errors regarding dependency?
+--------------------------------------------------------------------------
+
+Let's say you disabled Mage_Dataflow but enabled Mage_Catalog and the system is presenting you:
+
+    "Mage_Catalog" requires module "Mage_Dataflow"
+
+You have two possibilities:
+
+1. Edit app/etc/modules/Mage_All.xml and remove the appropriate dependencies.
+2. Edit your main index.php file and add a custom config model to `Mage::run()`
+
+```php
+Mage::run($mageRunCode, $mageRunType, array(
+    'config_model' => 'Zookal_Mock_Model_Config'
+));
+```
+
+The config model `Zookal_Mock_Model_Config` will automatically resolve invalid dependencies for disabled modules.
+
+
 Will there be a performance increase?
 -------------------------------------
 
--- src/app/code/community/Zookal/Mock/Model/Config.php
+++ src/app/code/community/Zookal/Mock/Model/Config.php
@@ -6,17 +6,30 @@
  * @copyright   Copyright (c) Zookal Pty Ltd
  * @license     OSL - Open Software Licence 3.0 | http://opensource.org/licenses/osl-3.0.php
  */
+
+/**
+ * Only usable in index.php:
+ * Mage::run($mageRunCode, $mageRunType, array(
+ * 'config_model' => 'Zookal_Mock_Model_Config'
+ * ));
+ *
+ * Class Zookal_Mock_Model_Config
+ */
 class Zookal_Mock_Model_Config extends Mage_Core_Model_Config
 {
     /**
-     * Key = active Module name => values inactive Module names whose dependency can be removed
+     * Key = active Module name => values inactive Module names whose dependency can be removed.
+     * If you find more please send me a PR.
+     * Maybe this config could also reside in a xml file.
      * @var array
      */
     protected $_dependencyLiars = array(
-        'Mage_Log'      => array('Mage_Customer'),
-        'Mage_Tax'      => array('Mage_Customer'),
-        'Mage_Catalog'  => array('Mage_Dataflow'),
-        'Mage_Customer' => array('Mage_Dataflow'),
+        'Mage_Reports'    => array('Mage_Sales', 'Mage_Customer'),
+        'Mage_Log'        => array('Mage_Customer'),
+        'Mage_Tax'        => array('Mage_Customer'),
+        'Mage_Newsletter' => array('Mage_Widget'),
+        'Mage_Catalog'    => array('Mage_Dataflow'),
+        'Mage_Customer'   => array('Mage_Dataflow'),
     );
 
     /**
