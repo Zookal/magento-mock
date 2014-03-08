41b2f3469d27698b71b63bf572da9d1be3626281
Cyrill Schumacher <cyrill@schumacher.fm>
2014-03-08 12:29:45 +1100

    Fixed typo

--- src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Tax/Class.php
+++ src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Tax/Class.php
@@ -2,7 +2,7 @@
 
 /**
  * @category    Zookal_Mock
- * @package     Helper
+ * @package     Model
  * @author      Cyrill Schumacher | {firstName}@{lastName}.fm | @SchumacherFM
  * @copyright   Copyright (c) Zookal Pty Ltd
  * @license     OSL - Open Software Licence 3.0 | http://opensource.org/licenses/osl-3.0.php
