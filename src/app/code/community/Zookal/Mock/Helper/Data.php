a27371ad43f1825dd144b00cc1ac018c328fbbaa
Cyrill Schumacher <cyrill@schumacher.fm>
2014-04-02 14:50:43 +1100

    Add Tests for Abtract Mock class, making that class more testable

--- src/app/code/community/Zookal/Mock/Helper/Data.php
+++ src/app/code/community/Zookal/Mock/Helper/Data.php
@@ -9,4 +9,36 @@
  */
 class Zookal_Mock_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
+    /**
+     * @var Mage_Core_Model_Store
+     */
+    protected $_store = null;
+
+    /**
+     * @param Mage_Core_Model_Store $store
+     */
+    public function __construct(Mage_Core_Model_Store $store = null)
+    {
+        $this->_store = $store;
+    }
+
+    /**
+     * @return Mage_Core_Model_Store
+     */
+    public function getStore()
+    {
+        if (null === $this->_store) {
+            $this->_store = Mage::app()->getStore();
+        }
+        return $this->_store;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isLogMethodEnabled()
+    {
+        return (int)$this->getStore()->getConfig('system/zookalmock/enable_method_log') === 1;
+    }
 }
