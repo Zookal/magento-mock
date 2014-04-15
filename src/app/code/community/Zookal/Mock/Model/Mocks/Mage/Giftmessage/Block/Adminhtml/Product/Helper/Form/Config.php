e4d02b49589d851b11c9819f7727b84ec145776e
Cyrill Schumacher <cyrill@schumacher.fm>
2014-04-15 11:55:38 +1000

    Added mock class Mage_GiftMessage_Block_Adminhtml_Product_Helper_Form_Config when GiftMessage module files has been removed

---
+++ src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Giftmessage/Block/Adminhtml/Product/Helper/Form/Config.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @category    Zookal_Mock
+ * @package     Model
+ * @author      Cyrill Schumacher | {firstName}@{lastName}.fm | @SchumacherFM
+ * @copyright   Copyright (c) Zookal Pty Ltd
+ * @license     OSL - Open Software Licence 3.0 | http://opensource.org/licenses/osl-3.0.php
+ */
+class Mage_GiftMessage_Block_Adminhtml_Product_Helper_Form_Config extends Mage_Adminhtml_Block_Catalog_Product_Helper_Form_Config
+{
+    /**
+     * Return nothing
+     *
+     * @return string
+     */
+    public function getElementHtml()
+    {
+        return '<strong>' . Mage::helper('zookal_mock')->__('Module Disabled!') . '</strong>';
+    }
+}
