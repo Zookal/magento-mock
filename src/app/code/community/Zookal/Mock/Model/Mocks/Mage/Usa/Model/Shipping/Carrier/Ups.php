61ba648e5fe77278463f01cdc25a4cddfac75aac
Cyrill Schumacher <cyrill@schumacher.fm>
2014-04-16 16:49:58 +1000

    Adding Mage_Usa fallback model. errors occur when loading system/config and files of Mage_Usa have been removed

---
+++ src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Usa/Model/Shipping/Carrier/Ups.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @category    Zookal_Mock
+ * @package     Model
+ * @author      Cyrill Schumacher | {firstName}@{lastName}.fm | @SchumacherFM
+ * @copyright   Copyright (c) Zookal Pty Ltd
+ * @license     OSL - Open Software Licence 3.0 | http://opensource.org/licenses/osl-3.0.php
+ */
+class Mage_Usa_Model_Shipping_Carrier_Ups extends Zookal_Mock_Model_Mocks_Abstract
+{
+
+}
