7ffffd8cc934a4c8a395241a079f6a48fa2726f1
Cyrill Schumacher <cyrill@schumacher.fm>
2014-04-25 20:47:22 +1000

    Add warning

--- src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Giftmessage/Block/Adminhtml/Product/Helper/Form/Config.php
+++ src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Giftmessage/Block/Adminhtml/Product/Helper/Form/Config.php
@@ -7,6 +7,12 @@
  * @copyright   Copyright (c) Zookal Pty Ltd
  * @license     OSL - Open Software Licence 3.0 | http://opensource.org/licenses/osl-3.0.php
  */
+
+/**
+ * Class Mage_GiftMessage_Block_Adminhtml_Product_Helper_Form_Config
+ * Do not change the class name, as it is needed for the autoloader because this class is somewhere in Magentos source code hardcoded :-(
+ * @see Zookal_Mock_Model_Observer::_setMockIncludePath
+ */
 class Mage_GiftMessage_Block_Adminhtml_Product_Helper_Form_Config extends Mage_Adminhtml_Block_Catalog_Product_Helper_Form_Config
 {
     /**
