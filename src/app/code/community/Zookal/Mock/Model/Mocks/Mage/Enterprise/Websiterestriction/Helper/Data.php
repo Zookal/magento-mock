e7dc3fa23ced1cdf1b1d3d3269bfe98b6170309e
Thomas <thomas@alternize.com>
2015-02-01 13:17:17 +0100

    mock the helper of Enterprise_Websiterestriction
    other enterprise extensions call this helper, for example the FPC crawler.

---
+++ src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Enterprise/Websiterestriction/Helper/Data.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @category    Zookal_Mock
+ * @package     Model
+ * @author      Cyrill Schumacher | {firstName}@{lastName}.fm | @SchumacherFM
+ * @copyright   Copyright (c) Zookal Pty Ltd
+ * @license     OSL - Open Software Licence 3.0 | http://opensource.org/licenses/osl-3.0.php
+ */
+
+/**
+ * Class Mage_Enterprise_Websiterestriction_Helper_Data
+ * Do not change the class name, as it is needed for the autoloader
+ * @see Zookal_Mock_Model_Observer::_setMockIncludePath
+ */
+class Mage_Enterprise_Websiterestriction_Helper_Data extends Zookal_Mock_Model_Mocks_Abstract
+{
+}
