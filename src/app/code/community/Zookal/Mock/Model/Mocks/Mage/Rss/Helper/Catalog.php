af63af1860a92d70fc19afb1b146b59a9a964473
Cyrill Schumacher <cyrill@schumacher.fm>
2015-06-04 11:48:13 +1000

    Add Mage_Rss_Helper_Catalog

---
+++ src/app/code/community/Zookal/Mock/Model/Mocks/Mage/Rss/Helper/Catalog.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @category    Zookal_Mock
+ * @package     Model
+ * @author      Cyrill Schumacher | {firstName}@{lastName}.fm | @SchumacherFM
+ * @copyright   Copyright (c) Zookal Pty Ltd
+ * @license     OSL - Open Software Licence 3.0 | http://opensource.org/licenses/osl-3.0.php
+ */
+
+/**
+ * Class Mage_Rss_Helper_Catalog
+ * Do not change the class name, as it is needed for the autoloader
+ * @see Zookal_Mock_Model_Observer::_setMockIncludePath
+ */
+class Mage_Rss_Helper_Catalog extends Zookal_Mock_Model_Mocks_Abstract
+{
+
+}
